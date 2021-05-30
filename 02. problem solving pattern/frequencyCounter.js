dd55c25e50b419f263d95d2f094593754a1824bc
sanat <sanat@coaxcreative.com>
2021-05-30 07:30:29 +0100

    changes in structure

--- 02. problem solving pattern/frequencyCounter.js
+++ 02. problem solving pattern/frequencyCounter.js
@@ -30,3 +30,31 @@ const  checkFunctionforIndex = (str, char) =>{
     }
 }
 console.log(anagramCheck('kreab','breek'));
+
+//  better approach not to use nested loop instead of it change it to breakdown the strcuture.
+
+const managewithfrequencyCounter  = (str1, str2) => {
+    if(str1.length !== str2.length) {
+        return false;
+    }
+    var obj1 = {};
+    var obj2 = {};
+    for(char of str1 ){// O(n)
+        obj1[char] = obj1[char]? obj1[char]+1 : 1;
+    }
+    // O(n)
+    for(char2 of str2){
+        obj2[char2] = obj2[char2]? obj1[char2]+1 : 1;
+    }
+    // objec like => {i:1,c:1,m:1,a:1,n:1} /=> {i:1,c:1,m:1,a:1,n:1}
+    console.log('obejc', obj1, obj2);
+    // O(n)
+    for(key in obj1) {
+        if(obj1[key] !== obj2[key]){
+            return false;
+        }
+    }
+    return true;
+}
+// O(n) + O(n) + O(n) => O(3n) => O(n)
+console.log(managewithfrequencyCounter('break','kreab'));
